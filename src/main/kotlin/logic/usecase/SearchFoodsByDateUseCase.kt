cf11bee962143fc0638f40efb8192a2a2a3c2421
nada <nadamansoursamir@gmail.com>
2025-04-15 23:17:40 +0200

    add search foods by date use case, and it's logic

---
+++ src/main/kotlin/logic/usecase/SearchFoodsByDateUseCase.kt
@@ -0,0 +1,53 @@
+package org.example.logic.usecase
+import org.example.logic.model.Meal
+import org.example.logic.repository.MealsRepository
+import org.example.logic.usecase.exceptions.IncorrectDateFormatException
+import org.example.logic.usecase.exceptions.MealsNotFoundForThisDateException
+import java.text.SimpleDateFormat
+import java.util.Date
+
+class SearchFoodsByDateUseCase(
+
+    private val mealsRepository: MealsRepository
+) {
+
+    fun dateFormat(date:String):Date{
+
+        return try {
+            val formatter = SimpleDateFormat("dd/MM/yyyy")
+            formatter.parse(date)
+        } catch (e: Exception) {
+            throw IncorrectDateFormatException("Incorrect Date Format")
+        }
+    }
+
+    fun SearchMealsByDate(date:String):List<Pair<String?,Long?>> {
+
+        val mealsByDate=mealsRepository.getAllMeals().filter { meal ->
+                meal.submitted == dateFormat(date)
+            }
+
+        if(mealsByDate.isEmpty()){
+                throw MealsNotFoundForThisDateException("No meals were found for the given date")
+            }
+
+        else {
+            return mealsByDate.map { meal ->
+                meal.name to meal.id
+            }
+        }
+
+
+    }
+
+
+
+    fun getMealDetailsById(id:Long):Meal?{
+        return mealsRepository.getAllMeals().find {
+            it.id==id
+        }
+    }
+
+
+
+}
