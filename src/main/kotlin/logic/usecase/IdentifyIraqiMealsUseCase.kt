a6eaa683863a3b39b514eb6fdb4eb82815bca3a4
Mohannad Ahmed <mohannad199344@gmail.com>
2025-04-15 16:48:33 +0200

    add IdentifyIraqiMealsUseCase with its logic

---
+++ src/main/kotlin/logic/usecase/IdentifyIraqiMealsUseCase.kt
@@ -0,0 +1,19 @@
+package org.example.logic.usecase
+
+import org.example.logic.model.Meal
+import org.example.logic.repository.MealsRepository
+
+class IdentifyIraqiMealsUseCase(
+    private val mealsRepository: MealsRepository
+) {
+    fun getMeals(): List<Meal> {
+        return mealsRepository.getAllMeals()
+            .filter(::isIraqiMeal)
+    }
+
+    private fun isIraqiMeal(meal: Meal): Boolean {
+        val hasIraqiTag = meal.tags?.any { it.equals("iraqi", ignoreCase = true) } ?: false
+        val isDescriptionContainsIraqWord = meal.description?.contains("Iraq", ignoreCase = true) ?: false
+        return hasIraqiTag && isDescriptionContainsIraqWord
+    }
+}
