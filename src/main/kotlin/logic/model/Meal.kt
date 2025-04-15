d1302bf109b2b63df867889f4d36aca621607cec
mohamedshemees <mohamedessampd@gmail.com>
2025-04-15 14:38:22 +0200

    updating models datatype

--- src/main/kotlin/logic/model/Meal.kt
+++ src/main/kotlin/logic/model/Meal.kt
@@ -1,18 +1,18 @@
 package org.example.logic.model
 
-import java.time.LocalDate
+import java.util.Date
 
 data class Meal(
-    val name:String,
-    val id: Long,
-    val minutes: String,
-    val contributorId: Long,
-    val submitted: LocalDate,
-    val tags:List<String>,
-    val nutrition: List<Nutrition>,
-    val noOfSteps: Any,
-    val steps:List<String>,
-    val description:String,
-    val ingredients:List<String>,
-    val noOfIngredients: Any,
+    val name:String?,
+    val id: Long?,
+    val minutes: String?,
+    val contributorId: Long?,
+    val submitted: Date,
+    val tags:List<String>?,
+    val nutrition: Nutrition?,
+    val numberOfSteps : Int?,
+    val steps:List<String>?,
+    val description:String?,
+    val ingredients:List<String>?,
+    val numberOfIngredients : Int?,
 )
