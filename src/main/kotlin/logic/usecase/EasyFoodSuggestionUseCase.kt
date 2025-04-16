406574fc581a1b7f98d9abc9b9ff4b457ec10a3e
mohamedshemees <mohamedessampd@gmail.com>
2025-04-16 21:07:56 +0200

    Rename EasyFoodSuggestionUseCase to GetEasyFoodSuggestionUseCase and update method to invoke()

--- src/main/kotlin/logic/usecase/EasyFoodSuggestionUseCase.kt
+++ src/main/kotlin/logic/usecase/EasyFoodSuggestionUseCase.kt
@@ -4,15 +4,17 @@ import org.example.logic.model.Meal
 import org.example.logic.repository.MealsRepository
 import kotlin.random.Random
 
-class EasyFoodSuggestionUseCase(private val mealsRepository: MealsRepository) {
-    fun getMeals():List<Meal> {
+class GetEasyFoodSuggestionUseCase(private val mealsRepository: MealsRepository) {
+    operator fun invoke():List<Meal> {
         return  mealsRepository.getAllMeals().filter(
             ::isPreparedInSixStepsOrFewer
         ).shuffled(Random).take(10)}
+
+    private fun isPreparedInSixStepsOrFewer(meal:Meal):Boolean{
+        return  ((meal.numberOfSteps ?: 0) <= 6)
+                && ((meal.minutes ?: 0) <= 30)
+                && ((meal.numberOfIngredients ?: 0) < 5)
+    }
 }
 
-private fun isPreparedInSixStepsOrFewer(meal:Meal):Boolean{
-    return  ((meal.numberOfSteps ?: 0) <= 6)
-            && ((meal.minutes ?: 0) <= 30)
-            && ((meal.numberOfIngredients ?: 0) < 5)
-}
+
