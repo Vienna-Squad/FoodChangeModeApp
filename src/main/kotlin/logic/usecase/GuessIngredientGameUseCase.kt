2bff36f1510598e651b703761ea13ed01ae9c46b
a7med naser <ad0112727@gmail.com>
2025-04-17 12:52:16 +0200

    update ingredient game use case

--- src/main/kotlin/logic/usecase/GuessIngredientGameUseCase.kt
+++ src/main/kotlin/logic/usecase/GuessIngredientGameUseCase.kt
@@ -10,26 +10,14 @@ class GuessIngredientGameUseCase(
 ) {
 
 
-//    fun guess(guess: String){
-//        if (!checkUserInput(guess,generateRandomMeal())) print("Wrong Answer")
-//        else{
-//            guess(guess)
-//        }
-//    }
-
-    private fun generateRandomMeal(): String {
-        return mealsRepository.getAllMeals().map(Meal::name).random()
-            ?: throw Exception("error in generate Random Meal")
-    }
-
-
-    private fun checkUserInput(ingredient: String, randomMealName: String?): Boolean {
+    fun generateRandomMeal(): String {
         return mealsRepository.getAllMeals()
-            .first { it.name == randomMealName }
-            .ingredients?.any { ingredientItem -> ingredientItem.contains(ingredient, ignoreCase = true) } == true
+            .filter { it.numberOfIngredients != 0 }
+            .map(Meal::name).random()
+            ?: throw IngredientRandomMealGenerationException("The Meal Not Contain Any Ingredients")
     }
 
-    private fun getIngredientListOptions(randomMealName: String?, randomChoose: Boolean): List<String?> {
+    fun generateIngredientListOptions(randomMealName: String?, randomChoose: Boolean): List<String?> {
 
         val correctOptions = mealsRepository.getAllMeals()
             .first { it.name == randomMealName }
@@ -60,61 +48,31 @@ class GuessIngredientGameUseCase(
         }
     }
 
-    fun showIngredientGuessGame(){
-
-        var score = 0
-        var counter = 0
-        var randomNumber = true
-        var correctGuess = true
-
-        while (correctGuess && counter < 15) {
-
-
-            val randomMealName = generateRandomMeal()
-            println("The Meal : $randomMealName")
-
-            val showUserList = getIngredientListOptions(randomMealName, randomNumber)
-            randomNumber = !randomNumber
-            println(showUserList)
-            println("1 for option 1 ### 2 for option 2 ### 3 for option 3")
-
-            // get User ingredient
-            print("Enter the Ingredient Input Number : ")
-            val input = readln().toIntOrNull()
-
-            val getUserInput = getOptionFromIngredientsList(showUserList, input?:-1)
-
-            if (checkUserInput(getUserInput, randomMealName)) {
-                println("Correct .....")
-                score += 1000
-                counter++
-            } else {
-                println("Your Score : $score")
-                println("failure try again later ...")
-                println("End Game ")
-                correctGuess = false
-            }
-
-        }
-    }
-    private fun getOptionFromIngredientsList(ingredients: List<String?>, option: Int): String {
+    fun getIngredientOptionByNumber(ingredientsOptions: List<String?>, option: Int): String {
         return when (option) {
             FIRST_OPTION -> {
-                ingredients[FIRST_OPTION_INDEX]
+                ingredientsOptions[FIRST_OPTION_INDEX]
             }
 
             SECOND_OPTION -> {
-                ingredients[SECOND_OPTION_INDEX]
+                ingredientsOptions[SECOND_OPTION_INDEX]
             }
 
             THIRD_OPTION -> {
-                ingredients[THIRD_OPTION_INDEX]
+                ingredientsOptions[THIRD_OPTION_INDEX]
             }
 
             else -> {
-                throw Exception("InValid Number")
+                throw IngredientUserInputException("InValid Input Number")
             }
-        } ?: throw Exception("Ingredients List is Null Or Empty")
+        } ?: throw IngredientsOptionsException("Ingredients List is Null Or Empty")
+    }
+
+    fun checkIngredientUserInput(ingredient: String, randomMealName: String?): Boolean {
+        return mealsRepository.getAllMeals()
+            .first { it.name == randomMealName }
+            .ingredients
+            ?.any { ingredientItem -> ingredientItem.contains(ingredient, ignoreCase = true) } == true
     }
 
     companion object {
@@ -132,5 +90,8 @@ class GuessIngredientGameUseCase(
 
 }
 
-open class IngredientGameException(message: MESSAGE): Exception(message)
-//error in generate Random Meal
+// instead Exception put App Exception
+open class IngredientGameException(message: MESSAGE) : Exception(message)
+class IngredientRandomMealGenerationException(message: String) : IngredientGameException(message)
+class IngredientUserInputException(message: String) : IngredientGameException(message)
+class IngredientsOptionsException(message: String) : IngredientGameException(message)
