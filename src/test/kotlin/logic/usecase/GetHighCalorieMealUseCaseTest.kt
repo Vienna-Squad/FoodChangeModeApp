d0a2849edc8c02cbdd91d6af4422095273a4d533
a7med naser <ad0112727@gmail.com>
2025-04-23 17:37:01 +0200

    update unit test of high calorie use case

--- src/test/kotlin/logic/usecase/GetHighCalorieMealUseCaseTest.kt
+++ src/test/kotlin/logic/usecase/GetHighCalorieMealUseCaseTest.kt
@@ -8,12 +8,12 @@ import org.example.logic.model.Meal
 import org.example.logic.model.Nutrition
 import org.example.logic.repository.MealsRepository
 import org.example.logic.usecase.GetHighCalorieMealUseCase
-import org.example.logic.usecase.exceptions.NullHighCalorieRandomMealException
-import org.example.logic.usecase.exceptions.NullRandomMealException
+import org.example.logic.usecase.exceptions.EmptyRandomMealException
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import java.util.Date
+import kotlin.test.assertEquals
 
 
 class GetHighCalorieMealUseCaseTest {
@@ -27,68 +27,102 @@ class GetHighCalorieMealUseCaseTest {
     }
 
     @Test
-    fun `getNameAndDescription should throw NullRandomMealException if name of meal is null or empty`() {
+    fun `getRandomHighCalorieMeal should throw EmptyRandomMealException when name of meal is null or empty`() {
         // stubs
-        every { mealsRepository.getAllMeals() } returns emptyList()
+        every { mealsRepository.getAllMeals() } returns listOf(
+            createMeal(
+                name = null,
+                description = "The meal with no description",
+                calorie = 0.0f
+            )
+        )
         // when & then
-        val exception = assertThrows<NullRandomMealException>{getHighCalorieMealUseCase.getNameAndDescription() }.apply {
-            assertThat(message).isEqualTo("The meal with more than 700 calorie in meal list")
+        assertThrows<EmptyRandomMealException> { getHighCalorieMealUseCase.getRandomHighCalorieMeal() }.apply {
+            assertThat(message).isEqualTo("the meal contain more than 700 calorie is not found in data")
         }
     }
+
     @Test
-    fun `getNameAndDescription should throw NullRandomMealException if description of meal is null or empty`() {
+    fun `getRandomHighCalorieMeal should throw EmptyRandomMealException when description of meal is null or empty`() {
         // stubs
         every { mealsRepository.getAllMeals() } returns listOf(
-            createMeal("chicken",null),
+            createMeal(name = "chicken", description = null, calorie = 750f),
         )
         // when & then
-        assertThrows<NullRandomMealException>{getHighCalorieMealUseCase.getNameAndDescription()}
+        assertThrows<EmptyRandomMealException> { getHighCalorieMealUseCase.getRandomHighCalorieMeal() }
     }
+
     @Test
-    fun `getNameAndDescription should throw NullRandomMealException if calories of meal is null or empty`() {
+    fun `getRandomHighCalorieMeal should throw EmptyRandomMealException when calories of meal is null or empty`() {
         // stubs
         every { mealsRepository.getAllMeals() } returns listOf(
-            createMeal("H","desc",null),
+            createMeal(name = "chicken", description = null, calorie = null),
         )
 
         // when & then
-        assertThrows<NullRandomMealException>{getHighCalorieMealUseCase.getNameAndDescription()}
+        assertThrows<EmptyRandomMealException> { getHighCalorieMealUseCase.getRandomHighCalorieMeal() }
     }
 
     @Test
-    fun `getNameAndDescription should return correct high calorie meal if the meal list contain it`() {
+    fun `getRandomHighCalorieMeal should throw EmptyRandomMealException when the list of meals contain meal with more than 700 calorie`() {
         // stubs
         every { mealsRepository.getAllMeals() } returns listOf(
-            createMeal("chicken","the good meal contain just chicken",600f),
-            createMeal("chicken","the good meal contain just chicken",700f),
-            createMeal("chicken with more calorie","the good meal contain just chicken",800f)
+            createMeal(
+                name = "chicken",
+                description = "the good meal contain just chicken",
+                calorie = 600f
+            ),
+            createMeal(
+                name = "chicken",
+                description = "the good meal contain just chicken",
+                calorie = 700f
+            ),
+            createMeal(
+                name = "chicken with more calorie",
+                description = "the good meal contain just chicken",
+                calorie = 500f
+            )
         )
-        // when
-        val result = getHighCalorieMealUseCase.getNameAndDescription()
-        // then
-        assertThat(result).isNotNull()
+        // when & then
+        assertThrows<EmptyRandomMealException> { getHighCalorieMealUseCase.getRandomHighCalorieMeal() }
     }
 
+
     @Test
-    fun `getMealDetailsByName should throw NullRandomMealException if the name of meal wrong`() {
-        // given
+    fun `getRandomHighCalorieMeal should return correct meal when the meal is valid`() {
+        // stubs
         every { mealsRepository.getAllMeals() } returns listOf(
-            createMeal("Tomato","hot chicken",600f),
-            createMeal("Hot Dog","hot chicken",700f)
+            createMeal(
+                name = "chicken",
+                description = "the good meal contain just chicken",
+                calorie = 600f
+            ),
+            createMeal(
+                name = "Hot Dog",
+                description = "the good meal contain just chicken",
+                calorie = 700f
+            ),
+            createMeal(
+                name = "chicken with more calorie",
+                description = "the good meal contain just chicken",
+                calorie = 800f
+            )
         )
-        val inputName = "chicken"
         // when
-        val result = getHighCalorieMealUseCase.getMealDetailsByName(inputName)
+        val result = getHighCalorieMealUseCase.getRandomHighCalorieMeal()
+        val expected = createMeal("chicken with more calorie", "the good meal contain just chicken", 800f)
+
         // then
-        assertThrows <NullHighCalorieRandomMealException>{
-            result
-        }
+        // assertEquals(result,expected)
+        assertEquals(result.toString(), expected.toString())
     }
+
+
 
 }
 
 
-fun createMeal(name: String?,description: String?,calorie: Float? = 800f): Meal{
+fun createMeal(name: String?, description: String?, calorie: Float?): Meal {
     return Meal(
         name = name,
         id = null,
