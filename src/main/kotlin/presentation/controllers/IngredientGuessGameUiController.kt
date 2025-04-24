9e5cd182b9269f26805e771053d8d47f49742bbc
a7med naser <ad0112727@gmail.com>
2025-04-24 13:43:29 +0200

    update Ingredient game ui and add viewer to show details and score of game

--- src/main/kotlin/presentation/controllers/IngredientGuessGameUiController.kt
+++ src/main/kotlin/presentation/controllers/IngredientGuessGameUiController.kt
@@ -1,14 +1,21 @@
 package org.example.presentation.controllers
 
 import org.example.logic.usecase.GuessIngredientGameUseCase
+import org.example.logic.usecase.IngredientGameDetails
 import org.example.logic.usecase.exceptions.IngredientsOptionsException
-import org.example.presentation.FoodViewer
-import org.example.presentation.UiController
+import org.example.utils.viewer.ExceptionViewer
+import org.example.utils.viewer.FoodExceptionViewer
+import org.example.utils.viewer.IngredientGameDetailsViewer
+import org.example.utils.viewer.ItemDetailsViewer
+import org.example.utils.viewer.utils.viewer.IngredientGameScoreViewer
+
 import org.koin.mp.KoinPlatform.getKoin
 
 class IngredientGuessGameUiController(
     private val guessIngredientGameUseCase: GuessIngredientGameUseCase = getKoin().get(),
-    private val viewer: FoodViewer = FoodViewer()
+    private val ingredientDetailsViewer: ItemDetailsViewer<IngredientGameDetails> = IngredientGameDetailsViewer(),
+    private val scoreViewer: IngredientGameScoreViewer = IngredientGameScoreViewer(),
+    private val exceptionViewer: ExceptionViewer = FoodExceptionViewer()
 ) : UiController {
     override fun execute() {
 
@@ -16,12 +23,7 @@ class IngredientGuessGameUiController(
             var counter = 1
             do {
                 val gameDetails = guessIngredientGameUseCase.getGameDetails()
-//                viewer.showMealDetails(gameDetails.meal)
-                println(gameDetails.mealName)
-                println("\nOptions : ${gameDetails.ingredients}\n")
-                println("\t(1) first Option")
-                println("\t(2) second Option")
-                println("\t(3) third Option\n")
+                ingredientDetailsViewer.viewDetails(gameDetails)
 
                 print("Enter the Ingredient Input Number : ")
                 val input = readln().toIntOrNull() ?: -1
@@ -30,15 +32,16 @@ class IngredientGuessGameUiController(
                     ingredientInputNumber = input
                 )
 
-                val result = guessIngredientGameUseCase.getScoreOfGame()
-                println("Score : $result , CorrectResults : $counter")
+                val score = guessIngredientGameUseCase.getScoreOfGame()
+                scoreViewer.viewDetails(score)
+
                 counter++
 
             } while (counter <= 15)
         } catch (e: IngredientsOptionsException) {
-            viewer.showExceptionMessage(e)
             val score = guessIngredientGameUseCase.getScoreOfGame()
-            println("Score : $score ")
+            exceptionViewer.viewExceptionMessage(e)
+            scoreViewer.viewDetails(score)
         }
         guessIngredientGameUseCase.endGame()
     }
