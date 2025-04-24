622c1137b1ad89fc12a2a403a0706eb474e31516
Mostafa Ibrahim <mostafa1996998@gmail.com>
2025-04-24 12:50:20 +0200

    Add tests for GetMealsOfCountryUseCase and MealByCountryUiController to ensure full coverage

--- src/main/kotlin/presentation/controllers/MealByCountryUiController.kt
+++ src/main/kotlin/presentation/controllers/MealByCountryUiController.kt
@@ -2,26 +2,30 @@ package org.example.presentation.controllers
 
 import org.example.logic.model.Meal
 import org.example.logic.usecase.GetMealsOfCountryUseCase
+import org.example.logic.usecase.exceptions.NoMealFoundException
+import org.example.utils.interactor.Interactor
+import org.example.utils.interactor.UserInteractor
+import org.example.utils.viewer.ExceptionViewer
+import org.example.utils.viewer.FoodExceptionViewer
 import org.example.utils.viewer.ItemsViewer
 import org.example.utils.viewer.MealsViewer
 import org.koin.mp.KoinPlatform.getKoin
 
 class MealByCountryUiController(
     private val getMealsOfCountryUseCase: GetMealsOfCountryUseCase = getKoin().get(),
-    private val viewer: ItemsViewer<Meal> = MealsViewer()
+    private val viewer: ItemsViewer<Meal> = MealsViewer(),
+    private val exceptionViewer: ExceptionViewer = FoodExceptionViewer(),
+    private val interactor: Interactor = UserInteractor()
 ) : UiController {
     override fun execute() {
-        print("Enter a  country to explore its meals: ")
-        val input = readln()
-        try {
-            val meals = getMealsOfCountryUseCase(input)
-            if (meals.isEmpty()) {
-                println("No meals found for '$input'.")
-            } else {
+        print("Enter a country to explore its meals: ")
+        val input =interactor.getInput()
+            try {
+                val meals = getMealsOfCountryUseCase(input)
                 viewer.viewItems(meals)
+            } catch (e: NoMealFoundException) {
+                exceptionViewer.viewExceptionMessage(e)
             }
-        } catch (e: Exception) {
-            println("Error: ${e.message}")
-        }
     }
 }
+
